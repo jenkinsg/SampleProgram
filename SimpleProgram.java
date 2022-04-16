2961d8064428be05c6f9ddc4d2888def87815ce8
githubjenkin <githubjenkins409@gmail.com>
2022-04-16 10:44:31 +0530

    first commit

---
+++ SimpleProgram.java
@@ -0,0 +1,11 @@
+class SimpleProgram {
+ public static void main(String args[]) {
+   System.out.println(" *  *  *");
+   System.out.println("  Sample Test Program ");
+   System.out.println(" -------------- ");
+   System.out.println(" -------------- ");
+  }
+}
+
+
+ 
